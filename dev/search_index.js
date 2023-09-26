aecf984b9a6655bc6227ba3956fba6003a042788
Documenter.jl <documenter@juliadocs.github.io>
2023-09-26 17:57:37 +0000

    build based on fef59b5

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = GeometryOptimization","category":"page"},{"location":"#GeometryOptimization","page":"Home","title":"GeometryOptimization","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for GeometryOptimization.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [GeometryOptimization]","category":"page"}]
+}
