c2b81f094a5ff9da63f11e7b558e304a452f8fc3
CedricTravelletti <cedrictravelletti@gmail.com>
2024-01-15 15:36:15 +0100

    Added state updating procedure.

--- src/optimization.jl
+++ src/optimization.jl
@@ -2,22 +2,33 @@
 # Note that by default all particles in the system are assumed optimizable.
 # IMPORTANT: Note that we always work in cartesian coordinates.
 =#
-
+using DFTK
 export minimize_energy!
 
 
+function update_state(original_system, new_system, state)
+    if bounding_box(original_system) != bounding_box(new_system)
+        return DFTK.DFTKState()
+    else
+        return state
+    end
+end
+    
 """ 
-By default we work in cartesian coordinaes.
+By default we work in cartesian coordinates.
 Note that internally, when optimizing the cartesian positions, atomic units 
 are used.
 """
-function Optimization.OptimizationFunction(system, calculator; kwargs...)
+function Optimization.OptimizationFunction(system, calculator; pressure=0.0, kwargs...)
     mask = not_clamped_mask(system)  # mask is assumed not to change during optim.
 
-    f = function(x::AbstractVector{<:Real}, p)
+    # TODO: Note that this function will dispatch appropriately when called with 
+    # a Component vector.
+    f = function(x, p)
         new_system = update_not_clamped_positions(system, x * u"bohr")
-        energy = AtomsCalculators.potential_energy(new_system, calculator; kwargs...)
-        austrip(energy)
+        state = update_state(system, new_system, calculator.state)
+        energy = AtomsCalculators.potential_energy(new_system, calculator; state, kwargs...)
+        austrip(energy) + pressure * calculator.state.scfres.basis.model.unit_cell_volume
     end
 
     g! = function(G::AbstractVector{<:Real}, x::AbstractVector{<:Real}, p)
@@ -34,10 +45,10 @@ function Optimization.OptimizationFunction(system, calculator; kwargs...)
     OptimizationFunction(f; grad=g!)
 end
 
-function minimize_energy!(system, calculator; solver=Optim.LBFGS(), kwargs...)
+function minimize_energy!(system, calculator; pressure=0.0, solver=Optim.LBFGS(), kwargs...)
     # Use current system parameters as starting positions.
     x0 = austrip.(not_clamped_positions(system)) # Optim modifies x0 in-place, so need a mutable type.
-    f_opt = OptimizationFunction(system, calculator)
+    f_opt = OptimizationFunction(system, calculator; pressure)
     problem = OptimizationProblem(f_opt, x0, nothing)  # Last argument needed in Optimization.jl.
     solve(problem, solver; kwargs...)
 end
