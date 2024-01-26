572598d85388211b41888e284ff43402a323fdc0
CedricTravelletti <cedrictravelletti@gmail.com>
2024-01-26 16:02:59 +0100

    Strain from deformation and corresponding tests.

--- src/strain.jl
+++ src/strain.jl
@@ -1,4 +1,4 @@
-export apply_voigt_strain
+export apply_voigt_strain, compute_voigt_strain
 
 function voigt_to_full(v)
 	[v[1] .5*v[6] .5*v[5];
@@ -10,12 +10,35 @@ function full_to_voigt(A)
 	[A[1, 1], A[2, 2], A[3, 3], 2. * A[2, 3], 2. * A[1, 3], 2. * A[1, 2]]
 end
 
+""" 
+Transform a bounding box in list of vectors form in matrix form 
+(box vectors in the rows).
+"""
+function bbox_to_matrix(bbox)
+	reduce(hcat, bbox)
+end
+	
+@doc raw"""
+Deform system by applying given strain. Unit cell vectors as well as atomic 
+positions are scaled. For a matrix of strains E, the matrix L of unit cell vectors 
+is scaled accodring to L' = (I + E) L. 
+Input strains should be given as a vector (Voigt notation).
+"""
 function apply_voigt_strain(system, strain)
-	deformation = I + voigt_to_full(strain)
-	new_lattice = eachrow(
-			      reduce(hcat, bounding_box(system))' * deformation)
+	deformation_tensor = I + voigt_to_full(strain)
+	new_lattice = eachcol(
+			      deformation_tensor * bbox_to_matrix(bounding_box(system)))
 	# Also deform coordinates, since internally do not work in fractional coordinates.
-	new_positions = [transpose(pos) * deformation for pos in position(system)]
+	new_positions = [deformation_tensor * pos for pos in position(system)]
 	new_generalized_positions = ComponentVector(atoms=new_positions , bounding_box=new_lattice)
 	update_positions(system, new_generalized_positions)
+end
+
+@doc raw"""
+Compute the strain (in Voigt notation) needed to deform the unit cell `bouding_box` into 
+`deformed_bounding_box`.
+"""
+function compute_voigt_strain(bbox, deformed_bbox)
+	strain_full = (bbox_to_matrix(deformed_bbox .- bbox)) / bbox_to_matrix(bbox)
+	full_to_voigt(strain_full)
 end
--- test/strain.jl
+++ test/strain.jl
@@ -1,6 +1,6 @@
 #= Test Geometry Optimization on an aluminium supercell.
 =#
-@testitem "Stain interface: test cell deformation under strain" setup=[TestCases] begin
+@testitem "Stain interface: cell deformation under strain" setup=[TestCases] begin
     using AtomsBase
     using Unitful
     using UnitfulAtomic
@@ -16,7 +16,7 @@
     @test [v[1] for v in bounding_box(new_silicon)] ≈ 2. * [v[1] for v in bounding_box(silicon)] rtol=1e-3
 end
 
-@testitem "Stain interface: test atoms positions deformation under strain" setup=[TestCases] begin
+@testitem "Stain interface: atoms positions deformation under strain" setup=[TestCases] begin
     using AtomsBase
     using Unitful
     using UnitfulAtomic
@@ -30,4 +30,22 @@ end
     strain = Vector([1., 0, 0, 0, 0, 0.])
     new_silicon = apply_voigt_strain(silicon, strain)
     @test [v[1] for v in position(new_silicon)] ≈ 2. * [v[1] for v in position(silicon)] rtol=1e-3
+end
+
+@testitem "Stain interface: strain from deformation" setup=[TestCases] begin
+    bbox = [[1., 0, 0],
+                   [0., 2., 0],
+                   [0., 0, 3]]
+    bbox_pert = [[1., 0, 0],
+                   [0., 4., 0],
+                   [0., 0, 3]]
+    strain = compute_voigt_strain(bbox, bbox_pert)
+    @test strain ≈ [0., 1., 0, 0, 0, 0] rtol=1e-6
+    
+    # Test with y-z rotation.
+    bbox_pert = [[6,0,0],
+                 [0,2,1],
+                 [0,1.5,3]]
+    strain = compute_voigt_strain(bbox, bbox_pert)
+    @test strain ≈ [5., 0, 0, 1., 0, 0] rtol=1e-6
 end
