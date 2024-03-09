2079a6e1af2c400c463b448865f68fe14e60425f
CedricTravelletti <cedrictravelletti@gmail.com>
2024-03-09 17:35:08 +0100

    Fixed formatting.

--- src/strain.jl
+++ src/strain.jl
@@ -1,12 +1,12 @@
 
 function voigt_to_full(v)
-	[v[1] .5*v[6] .5*v[5];
-	 .5*v[6] v[2] .5*v[4];
-	 .5*v[5] .5*v[4] v[3]]
+    [v[1] .5*v[6] .5*v[5];
+    .5*v[6] v[2] .5*v[4];
+    .5*v[5] .5*v[4] v[3]]
 end
 
 function full_to_voigt(A)
-	[A[1, 1], A[2, 2], A[3, 3], 2. * A[2, 3], 2. * A[1, 3], 2. * A[1, 2]]
+    [A[1, 1], A[2, 2], A[3, 3], 2. * A[2, 3], 2. * A[1, 3], 2. * A[1, 2]]
 end
 
 """ 
@@ -14,7 +14,7 @@ Transform a bounding box in list of vectors form in matrix form
 (box vectors in the columns).
 """
 function bbox_to_matrix(bbox)
-	reduce(hcat, bbox)
+    reduce(hcat, bbox)
 end
 
 @doc raw"""
@@ -22,6 +22,6 @@ Compute the strain (in Voigt notation) needed to deform the unit cell `bouding_b
 `deformed_bounding_box`.
 """
 function compute_voigt_strain(bbox, deformed_bbox)
-	strain_full = (bbox_to_matrix(deformed_bbox .- bbox)) / bbox_to_matrix(bbox)
-	full_to_voigt(strain_full)
+    strain_full = (bbox_to_matrix(deformed_bbox .- bbox)) / bbox_to_matrix(bbox)
+    full_to_voigt(strain_full)
 end
