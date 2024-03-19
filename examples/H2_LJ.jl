b2d33fed5a74c1e40f4abfaccbd4c3964783d8cd
CedricTravelletti <cedrictravelletti@gmail.com>
2024-03-19 18:34:40 +0100

    Updated examples.

--- examples/H2_LJ.jl
+++ examples/H2_LJ.jl
@@ -1,5 +1,4 @@
-#= Test Geometry Optimization on an aluminium supercell.
-=#
+using Printf
 using LinearAlgebra
 using EmpiricalPotentials
 using Unitful
@@ -14,10 +13,9 @@ bounding_box = 10.0u"angstrom" .* [[1, 0, 0.], [0., 1, 0], [0., 0, 1]]
 atoms = [:H => [0, 0, 0.0]u"bohr", :H => [0, 0, 1.9]u"bohr"]
 system = periodic_system(atoms, bounding_box)
 
-lj = LennardJones(-1.17u"hartree", 0.743u"angstrom", 1, 1, 0.6u"nm")
+calculator = LennardJones(-1.17u"hartree", 0.743u"angstrom", 1, 1, 0.6u"nm")
 
 solver = OptimizationOptimJL.LBFGS()
-jk
 optim_options = (; solver, f_tol=1e-10, g_tol=1e-5, iterations=30,
                  show_trace=true, store_trace = true, allow_f_increases=true)
 
