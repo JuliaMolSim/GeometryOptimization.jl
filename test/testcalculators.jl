0f587268f3a0b93fe38e128ba85a237779fd30f1
Michael F. Herbst <info@michael-herbst.com>
2024-08-02 20:48:06 +0200

    Fix tests

---
+++ test/testcalculators.jl
@@ -0,0 +1,17 @@
+# Create a dummy AtomsCalculator to test the geometry optimization interfcae.
+@testsetup module TestCalculators
+    using AtomsCalculators
+    AC = AtomsCalculators
+
+    struct DummyCalc end
+    AC.energy_unit(::DummyCalc) = u"hartree"
+    AC.length_unit(::DummyCalc) = u"bohr"
+
+    AC.@generate_interface function AC.potential_energy(system, calc::DummyCalc; kwargs...)
+        AC.zero_energy(calc)
+    end
+
+    AC.@generate_interface function AC.forces(system, calc::DummyCalc; kwargs...)
+        AC.zero_forces(system, calc)
+    end
+end
